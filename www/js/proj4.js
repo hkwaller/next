15df89b668ce34c7d7e0cec6d44471baf031eb24
Jacob Waller <jaco@spotify.com>
2015-03-06 22:21:05 +0100

    Add note on how to fetch closest stations from the ruter API

--- www/js/proj4.js
+++ www/js/proj4.js
@@ -6,3 +6,6 @@ var lng = 10.734738;
 var xy = proj4('EPSG:25832', { x: lng, y: lat })
 
 console.log(xy);
+
+// Fetch closest stations: http://reis.ruter.no/reisrest/stop/getcloseststopsbycoordinates/?coordinates=(x=596948,y=6645178)&proposals=7
+// NB. xy above must be piped through Math.round before being used in the api request above.
