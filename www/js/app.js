318cb80c96695b7044614cee6a64502cb65bab88
Hannes Waller <hkwaller@gmail.com>
2015-03-06 22:14:35 +0100

    adapt design

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('next', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,30 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    controller: 'MainCtrl',
+    templateUrl: 'templates/start.html'
+  })
+  
+   $urlRouterProvider.otherwise("/");
+
+})
+
+.controller('MainCtrl', function($scope, $ionicSlideBoxDelegate) {
+    $scope.lines = [
+        {"number":17, "destination":"Rikshospitalet", "time":2, "class":"line-17"},
+        {"number":18, "destination":"Rikshospitalet", "time":4, "class":"line-18"},
+        {"number":37, "destination":"Ullevål Stadion", "time":6, "class":"line-37"},
+        {"number":54, "destination":"Sinsen", "time":7, "class":"line-54"},
+        {"number":17, "destination":"Rikshospitalet", "time":9, "class":"line-17"}
+    ];
+    
+    $scope.stations = [
+        "Adamstuen", "Stensgata", "Colletts Gate", "Bislett"
+    ];
+    
+});
