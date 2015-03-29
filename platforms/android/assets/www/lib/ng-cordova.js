e678eac6adc91e97cbe6688979f2bdaa06d6830e
Hannes Waller <hkwaller@gmail.com>
2015-03-29 20:10:36 +0200

    fixed ios & android setup

---
+++ platforms/android/assets/www/lib/ng-cordova.js
@@ -0,0 +1,110 @@
+
+angular.module('ngCordova', [
+  'ngCordova.plugins'
+]);
+angular.module('ngCordova.plugins', [	 'geolocation',	 'statusbar']);//#### Begin Individual Plugin Code ####// install   :     cordova plugin add org.apache.cordova.geolocation
+// link      :     https://github.com/apache/cordova-plugin-geolocation/blob/master/doc/index.md
+
+angular.module('ngCordova.plugins.geolocation', [])
+
+  .factory('$cordovaGeolocation', ['$q', function ($q) {
+
+    return {
+      getCurrentPosition: function (options) {
+        var q = $q.defer();
+
+        navigator.geolocation.getCurrentPosition(function (result) {
+          q.resolve(result);
+        }, function (err) {
+          q.reject(err);
+        }, options);
+
+        return q.promise;
+      },
+
+      watchPosition: function (options) {
+        var q = $q.defer();
+
+        var watchID = navigator.geolocation.watchPosition(function (result) {
+          q.notify(result);
+        }, function (err) {
+          q.reject(err);
+        }, options);
+
+        q.promise.cancel = function () {
+          navigator.geolocation.clearWatch(watchID);
+        };
+
+        q.promise.clearWatch = function (id) {
+          navigator.geolocation.clearWatch(id || watchID);
+        };
+
+        q.promise.watchID = watchID;
+
+        return q.promise;
+      },
+
+      clearWatch: function (watchID) {
+        return navigator.geolocation.clearWatch(watchID);
+      }
+    };
+  }]);
+// install   :      cordova plugin add org.apache.cordova.statusbar
+// link      :      https://github.com/apache/cordova-plugin-statusbar/blob/master/doc/index.md
+
+angular.module('ngCordova.plugins.statusbar', [])
+
+  .factory('$cordovaStatusbar', [function () {
+
+    return {
+      overlaysWebView: function (bool) {
+        return StatusBar.overlaysWebView(!!bool);
+      },
+
+      style: function (style) {
+        switch (style) {
+          // Default
+          case 0:
+            return StatusBar.styleDefault();
+
+          // LightContent
+          case 1:
+            return StatusBar.styleLightContent();
+
+          // BlackTranslucent
+          case 2:
+            return StatusBar.styleBlackTranslucent();
+
+          // BlackOpaque
+          case 3:
+            return StatusBar.styleBlackOpaque();
+
+          default:
+            return StatusBar.styleDefault();
+        }
+      },
+
+      // supported names:
+      // black, darkGray, lightGray, white, gray, red, green,
+      // blue, cyan, yellow, magenta, orange, purple, brown
+      styleColor: function (color) {
+        return StatusBar.backgroundColorByName(color);
+      },
+
+      styleHex: function (colorHex) {
+        return StatusBar.backgroundColorByHexString(colorHex);
+      },
+
+      hide: function () {
+        return StatusBar.hide();
+      },
+
+      show: function () {
+        return StatusBar.show();
+      },
+
+      isVisible: function () {
+        return StatusBar.isVisible;
+      }
+    };
+  }]);
