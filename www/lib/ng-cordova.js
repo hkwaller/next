05e549f697b3cfee232ef33b8d148b5fd0ef745d
Hannes Waller <hkwaller@gmail.com>
2015-03-29 20:09:17 +0200

    new cordova build

--- www/lib/ng-cordova.js
+++ www/lib/ng-cordova.js
@@ -2,7 +2,7 @@
 angular.module('ngCordova', [
   'ngCordova.plugins'
 ]);
-angular.module('ngCordova.plugins', [	 'geolocation']);//#### Begin Individual Plugin Code ####// install   :     cordova plugin add org.apache.cordova.geolocation
+angular.module('ngCordova.plugins', [	 'geolocation',	 'statusbar']);//#### Begin Individual Plugin Code ####// install   :     cordova plugin add org.apache.cordova.geolocation
 // link      :     https://github.com/apache/cordova-plugin-geolocation/blob/master/doc/index.md
 
 angular.module('ngCordova.plugins.geolocation', [])
@@ -46,6 +46,65 @@ angular.module('ngCordova.plugins.geolocation', [])
 
       clearWatch: function (watchID) {
         return navigator.geolocation.clearWatch(watchID);
+      }
+    };
+  }]);
+// install   :      cordova plugin add org.apache.cordova.statusbar
+// link      :      https://github.com/apache/cordova-plugin-statusbar/blob/master/doc/index.md
+
+angular.module('ngCordova.plugins.statusbar', [])
+
+  .factory('$cordovaStatusbar', [function () {
+
+    return {
+      overlaysWebView: function (bool) {
+        return StatusBar.overlaysWebView(!!bool);
+      },
+
+      style: function (style) {
+        switch (style) {
+          // Default
+          case 0:
+            return StatusBar.styleDefault();
+
+          // LightContent
+          case 1:
+            return StatusBar.styleLightContent();
+
+          // BlackTranslucent
+          case 2:
+            return StatusBar.styleBlackTranslucent();
+
+          // BlackOpaque
+          case 3:
+            return StatusBar.styleBlackOpaque();
+
+          default:
+            return StatusBar.styleDefault();
+        }
+      },
+
+      // supported names:
+      // black, darkGray, lightGray, white, gray, red, green,
+      // blue, cyan, yellow, magenta, orange, purple, brown
+      styleColor: function (color) {
+        return StatusBar.backgroundColorByName(color);
+      },
+
+      styleHex: function (colorHex) {
+        return StatusBar.backgroundColorByHexString(colorHex);
+      },
+
+      hide: function () {
+        return StatusBar.hide();
+      },
+
+      show: function () {
+        return StatusBar.show();
+      },
+
+      isVisible: function () {
+        return StatusBar.isVisible;
       }
     };
   }]);
