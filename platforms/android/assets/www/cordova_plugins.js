107606f8ffb9f9a94aef079b99fb5c0bc5c2cf53
hkwaller <hkwaller@gmail.com>
2017-04-04 21:23:17 +0200

    rebuild android

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -46,6 +46,19 @@ module.exports = [
             "cordova.plugins.Keyboard"
         ],
         "runs": true
+    },
+    {
+        "id": "cordova-plugin-x-toast.Toast",
+        "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+        "pluginId": "cordova-plugin-x-toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "id": "cordova-plugin-x-toast.tests",
+        "file": "plugins/cordova-plugin-x-toast/test/tests.js",
+        "pluginId": "cordova-plugin-x-toast"
     }
 ];
 module.exports.metadata = 
@@ -58,7 +71,8 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "3.2.2",
     "cordova-plugin-statusbar": "2.1.3",
     "cordova-plugin-whitelist": "1.2.2",
-    "ionic-plugin-keyboard": "2.2.1"
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-x-toast": "2.5.2"
 };
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/filter.js
+++ platforms/android/assets/www/js/filter.js
@@ -1,25 +1,9 @@
 angular.module('next.filters', [])
 
-.filter('favouriteFilter', function() {
-    return function(input) {
-        return (input || []).filter(function(station) {
-            return station.Preference > 0;
-        });
-    }
-})
-
-.filter('noFavouriteFilter', function() {
-    return function(input) {
-        return (input || []).filter(function(station) {
-            return station.Preference === 0;
-        });
-    }
-})
-
 .filter('detailFilter', function() {
     return function(input) {
         return (input || []).filter(function(line) {
-            return line.LineRef.length <= 3 && line.MinutesToDeparture < 20 && line.LineRef < 500;
+            return line.PublishedLineName.length <= 3 && line.MinutesToDeparture < 20;
         });
     }
 })
